4d9c07cf85618371a61386aeaed611c98ae1a8f9
supersuper2 <sing1883@algonquinlive.com>
2025-06-08 18:18:19 -0400

    Added cloud init and vm config

--- main.tf
+++ main.tf
@@ -21,8 +21,14 @@ provider "azurerm" {
   features {}
 }
 
-provider "cloudinit" {
-  # Configuration options
+data "cloudinit_config" "apache" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = file("${path.module}/init.sh")
+  }
 }
 
 #variables
@@ -115,4 +121,34 @@ resource "azurerm_network_interface" "main" {
 resource "azurerm_network_interface_security_group_association" "main" {
   network_interface_id      = azurerm_network_interface.main.id
   network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "${var.labelPrefix}-A05-VM"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.main.id
+  ]
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "${var.labelPrefix}-A05-OSDisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  custom_data = data.cloudinit_config.apache.rendered
 }
